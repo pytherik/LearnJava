cc9b649a9e382270f3126410a3f370b747c120b2
pytherik <erikberndt@gmx.net>
2023-09-24 15:49:53 +0200

    objects as arguments

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
   public static void main(String [] args) {
+    Garage garage = new Garage();
+    Car car = new Car("Trabi");
+    Car car2 = new Car("Bully");
 
+    garage.park(car);
+    garage.park(car2);
   }
 }
