8b86f14d54a229a3d083ae4dd5b4fc287222d91d
pytherik <erikberndt@gmx.net>
2023-09-24 22:18:30 +0200

    fix stupidity

--- src/HybridCar.java
+++ src/HybridCar.java
@@ -1,9 +1,10 @@
-public class HybridCar extends Car{
+public class HybridCar extends Car {
   double avgKmPerLitre;
   int batterySize;
   int cylinders;
   HybridCar(String description, double avgKmPerLitre, int batterySize, int cylinders) {
     super(description);
+//    this.description = description;
     this.avgKmPerLitre = avgKmPerLitre;
     this.batterySize = batterySize;
     this.cylinders = cylinders;
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
   public static void main(String [] args) {
 
-    HybridCar hCar = new HybridCar("Ein hCar", 12.3, 23, 4);
+    HybridCar hCar = new HybridCar(
+            "Ein hCar", 12.3, 23, 4);
     hCar.drive();
   }
 }
