b075bba7e10c490b35d98b1319c862ef14ca9b51
pytherik <erikberndt@gmx.net>
2023-09-24 18:55:54 +0200

    copy objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
   public static void main(String [] args) {
+    Car car1 = new Car("Volkswagen", "Bully", 1980);
+    Car car2 = new Car(car1);
 
+    //    Car car2 = new Car("Trabant", "Standard", 1975);
+    //    car2.copy(car1);
+
+
+
+    System.out.println(car1);
+    System.out.println(car2);
+    System.out.println();
+    System.out.println(car1.getMake());
+    System.out.println(car1.getModel());
+    System.out.println(car1.getYear());
+    System.out.println();
+    System.out.println(car2.getMake());
+    System.out.println(car2.getModel());
+    System.out.println(car2.getYear());
   }
 }
