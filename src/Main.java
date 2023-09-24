5807f1964dd0eeb6cc257b58039e522a52391771
pytherik <erikberndt@gmx.net>
2023-09-24 15:40:06 +0200

    array of objects

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
-  public static void main(String [] args) {
+  public static void main(String[] args) {
+    Food food1 = new Food("pizza");
+    Food food2 = new Food("chips");
+    Food food3 = new Food("chocolate");
 
+    Food[] refrigerator = {food1, food2, food3};
+
+//    Food[] refrigerator = new Food[3];
+
+//    refrigerator[0] = food1;
+//    refrigerator[1] = food2;
+//    refrigerator[2] = food3;
+
+    for (Food food : refrigerator) System.out.println(food);
   }
 }
