450f46daa17848b391345308f13e52bb971ab5e3
pytherik <erikberndt@gmx.net>
2023-09-25 06:39:04 +0200

    add rand

--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -1,13 +1,32 @@
+import java.util.Random;
+
 public class SwitchCase {
   public static void main(String [] args) {
-    String day = "Sunday";
+    Random rand = new Random();
+    String[] days = {"mo", "di", "mi", "do", "fr", "sa", "so"};
+    String day = days[rand.nextInt(days.length)];
 
     switch(day) {
-      case "Sunday":
+      case "so":
         System.out.println("Es ist Sonntag!");
         break;
-      case "Monday":
+      case "mo":
         System.out.println("Es ist Montag");
+        break;
+      case "di":
+        System.out.println("Es ist Dienstag!");
+        break;
+      case "mi":
+        System.out.println("Es ist Mittwoch");
+        break;
+      case "do":
+        System.out.println("Es ist Donnerstag!");
+        break;
+      case "fr":
+        System.out.println("Es ist Freitag");
+        break;
+      case "sa":
+        System.out.println("Es ist Sonnabend!");
         break;
       default:
         System.out.println("Heute ist kein Tag!");
