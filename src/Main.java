3eb150ea5841cf485ab38be66d3e0310c80b0fa4
Erik Berndt <erikberndt@gmx.net>
2023-09-27 10:38:05 +0200

    file Object

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
+import java.io.File;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
+        // file = an abstract representation of file and directory pathnames
+
+        File file = new File("./secret_message.txt");
+        
+        if (file.exists()) {
+            System.out.println("ich existiere!");
+        }
     }
 }
